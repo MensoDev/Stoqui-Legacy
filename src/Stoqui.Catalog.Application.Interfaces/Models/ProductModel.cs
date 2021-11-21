6a7efa8b33dd2f3e9081ddd4babcfa3fcb23f4a7
Emerson Trindade <emersont@outlook.com.br>
2021-11-21 16:31:14 -0300

    created: Created a Application.Interfaces project
    Project created with data management interfaces

---
+++ src/Stoqui.Catalog.Application.Interfaces/Models/ProductModel.cs
@@ -0,0 +1,38 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace Stoqui.Catalog.Application.Interfaces.Models
+{
+    public class ProductModel : Notifiable<Notification>
+    {
+        public ProductModel(Guid id, DateTime registrationDate, string name, string description)
+        {
+            Id = id;
+            RegistrationDate = registrationDate;
+            Name = name;
+            Description = description;
+        }
+
+        public ProductModel()
+        {
+            Name = string.Empty;
+            Description = string.Empty;
+        }
+
+        public Guid Id { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public void Validate()
+        {
+            var contract = new Contract<Notification>()
+                .IsNotEmpty(Name, "The Product Name is required")
+                .IsNotEmpty(Description, "The Product Description is required");
+
+            AddNotifications(contract);
+          
+        }
+
+    }
+}
---
+++ src/Stoqui.Catalog.Application.Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Stoqui.Catalog.Domain.Entities;
+using Stoqui.Kernel.Domain.Data;
+
+namespace Stoqui.Catalog.Application.Interfaces.Repositories;
+
+public interface IProductRepository : IRepository<Product>
+{
+    ValueTask AddProductAsync(Product product);
+    ValueTask<Product> GetProductByIdAsync(Guid productId);
+
+    void DeleteProduct(Product product);
+    void UpdateProduct(Product product);
+}
+
