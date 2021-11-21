3f6689d3824c199eeb67eca79938f6229372ffe0
Emerson Trindade <emersont@outlook.com.br>
2021-11-21 16:27:32 -0300

    created: Created a Generic Repository and Generic UnitOfWork

---
+++ src/Stoqui.Kernel.Domain/Data/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using Stoqui.Kernel.Domain.Objects;
+
+namespace Stoqui.Kernel.Domain.Data;
+
+public interface IRepository<T> : IDisposable where T : class, IAggregateRoot
+{
+    public IUnitOfWork UnitOfWork { get; init; }
+}
+
