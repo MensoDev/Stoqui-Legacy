53316dfef68b54f97f89bd9ecaac34125d3085fb
Emerson Trindade <emersont@outlook.com.br>
2021-11-21 16:32:55 -0300

    created: implemented a Application.Interfaces in Application layer

---
+++ src/Stoqui.Catalog.Application/Profiles/ProductProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Stoqui.Catalog.Application.Interfaces.Models;
+using Stoqui.Catalog.Domain.Entities;
+
+namespace Stoqui.Catalog.Application.Profiles;
+
+public class ProductProfile : Profile
+{
+    public ProductProfile()
+    {
+        CreateMap<Product, ProductModel>();
+    }
+}
+
---
+++ src/Stoqui.Catalog.Application/Services/ProductAppService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Stoqui.Catalog.Application.Interfaces.Models;
+using Stoqui.Catalog.Application.Interfaces.Repositories;
+using Stoqui.Catalog.Application.Interfaces.Services;
+using Stoqui.Catalog.Domain.Entities;
+
+namespace Stoqui.Catalog.Application.Services;
+
+public class ProductAppService : IProductAppService
+{
+
+    private readonly IMapper _mapper;
+    private readonly IProductRepository _productRepository;
+
+    public ProductAppService(IMapper mapper, IProductRepository productRepository)
+    {
+        _mapper = mapper;
+        _productRepository = productRepository;
+    }
+
+    public async ValueTask<bool> RegisterProductAsync(ProductModel model)
+    {
+        model.Validate();
+        if (!model.IsValid) 
+        {
+            //TODO: Register Notifications
+            return false;
+        }
+
+        var product = new Product(model.Name, model.Description);
+
+        //TODO: Show Events Here
+
+        await _productRepository.AddProductAsync(product);
+        return await _productRepository.UnitOfWork.CommitAsync();
+    }
+
+    public void Dispose()
+    {        
+        GC.SuppressFinalize(this);
+    }
+}
+
